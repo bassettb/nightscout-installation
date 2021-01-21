a955b5b826420be12c7c932ae6ba61dcd911cbb6
bassettb <bryanbassett@gmail.com>
2021-01-21 10:17:00 -0500

    fixes for AAPS

--- nightscout.sh
+++ nightscout.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-cd /opt/app
 export API_SECRET=[SECRET]
-export MONGO_CONNECTION=mongodb://localhost/patientname
-# Use localhost if using ngninx proxy.  Otherwise, use your hostname
+export MONGO_CONNECTION=mongodb://localhost/admin
+export MONGODB_URI=mongodb://localhost/admin
 export HOSTNAME=127.0.0.1
 export PORT=1337
 export DEVICESTATUS_ADVANCED=true
-export ENABLE="careportal iob"
+export ENABLE="careportal boluscalc food bwp cage sage iage iob cob basal ar2 rawbg pushover bgi pump openaps"
 export ALARM_TYPES=predict
 export AUTH_DEFAULT_ROLES=denied
-node server.js
+export INSECURE_USE_HTTP=true
+export DISPLAY_UNITS=mg/dl
+export PUMP_FIELDS="reservoir battery clock"
+cd /opt/app
+node /opt/app/server.js
